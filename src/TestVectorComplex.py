8cb2c520e875fdee97636b7259e2278b48010568
Stilink <stilink306@gmail.com>
2020-02-09 22:04:08 -0500

    ~

--- src/TestVectorComplex.py
+++ src/TestVectorComplex.py
@@ -1,11 +1,15 @@
 import ComplexCalculator
 import VectorComplexCalculator
 import TestComplexCalculator
+import unittest
+import random
 
 # Casos para los numeros
 cases = ["Positivos", "Negativos", "Positivo - Negativo", "Negativo - Positivo"]
 
 
+# Tamaño por defecto del vector para las pruebas
+n = 100
 # Generador de vectores usados dentro de las pruebas
 def generatorOfVectors(type, n):
     vector_to_return = []
@@ -13,6 +17,58 @@ def generatorOfVectors(type, n):
         vector_to_return.append(TestComplexCalculator.generatorOfComplex(type))
     return vector_to_return
 
+# Generador de un numero aleatorio necesario en algunos metodos
+def generatorRandomNumber(type):
+    comp = TestComplexCalculator.generatorOfComplex(type)[0]
+    return comp
+
 
 # Clase encargada de los test a la calculadora de vectores complejos
 class TestVectorComplexCalculator(unittest.TestCase):
+    # Test de la suma de vectores
+    def test_add_vector(self) :
+        for case in cases:
+            vector1 = generatorOfVectors(case,n)
+            vector2 = generatorOfVectors(case,n)
+            vector_result = VectorComplexCalculator.addVectors(vector1, vector2)
+            vector_to_compare = []
+            for i in range(n):
+                vector_to_compare.append(ComplexCalculator.add(vector1[i],vector2[i]))
+            self.assertEqual(vector_result, vector_to_compare)
+
+    # Test del metodo que retorna el inverso aditivo de un vector
+    def test_inverse(self):
+        for case in cases:
+            vector1 = generatorOfVectors(case,n)
+            vector_result = VectorComplexCalculator.inverse(vector1)
+            vector_to_compare = []
+            for i in range(n):
+                vector_to_compare.append((-1*vector1[i][0], -1*vector1[i][1]))
+            self.assertEqual(vector_result, vector_to_compare)
+
+    # Test de la multiplicación de un escalar por un vector
+    def test_scalar_product(self):
+        for case in cases:
+            number = generatorRandomNumber(random.choice(cases))
+            vector1 = generatorOfVectors(case,n)
+            vector_result = VectorComplexCalculator.multiplyScalar(number, vector1)
+            vector_to_compare =[]
+            for i in range(n):
+                vector_to_compare.append((number*vector1[i][0], number*vector1[i][1]))
+            self.assertEqual(vector_result, vector_to_compare)
+    
+    # Test para el conjugado de un vector
+    def test_conjugate(self):
+        for case  in cases:
+            vector1 = generatorOfVectors(case, n)
+            vector_result = VectorComplexCalculator.conjugateVector(vector1)
+            vector_to_compare = []
+            for i in range(n):
+                vector_to_compare.append(ComplexCalculator.conjugate(vector1[i]))
+            self.assertEqual(vector_result, vector_to_compare)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+            
--- src/VectorComplexCalculator.py
+++ src/VectorComplexCalculator.py
@@ -33,7 +33,7 @@ def inverse(a):
 def multiplyScalar(c, vector):
     vector_result = []
     for i in range(len(vector)):
-        result = ComplexCalculator.multiply(c, vector[i])
+        result = ComplexCalculator.multiply((c,0), vector[i])
         vector_result.append(result)
     return vector_result
 
